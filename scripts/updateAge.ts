06f63e73042ea3cb77d4fa3d3ade9c3e66fa5683
Mikkel <mikkelelvers@outlook.com>
2024-11-02 02:17:00 +0100

    Changed birthday

--- scripts/updateAge.ts
+++ scripts/updateAge.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 const calculateAge = (): number => {
-    return new Date(Date.now() - new Date("2006-08-11").getTime()).getFullYear() - 1970;
+    return new Date(Date.now() - new Date("2006-11-08").getTime()).getFullYear() - 1970;
 };
 
 const updateReadme = (newAge: number) => {
